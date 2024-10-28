711f269444eaabdf85ac31ad73e6c1568b5886c1
BleronaIdrizi <58502639+BleronaIdrizi@users.noreply.github.com>
2024-10-28 18:46:15 +0100

    Update vite configs on build.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/*', // Burimi i skedarëve që do të kopjohen
+          dest: 'src/assets'   // Destinacioni në dosjen 'dist'
+        }
+      ]
+    })
+  ]
+});
